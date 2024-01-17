841c4ea9245a756d36a3dcb59e678a0edb6646d8
Rob <rverplanke@gmail.com>
2024-01-17 19:34:43 +0100

    add prettyPrint vizualization code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import getNumbers from './modules/data-generator/randomNr.js';
 import Tree from './modules/bst.js';
+import prettyPrint from './prettyPrint.js';
 
 // Amount of random numbers
 const AMOUNT = 20;
@@ -7,4 +8,5 @@ const AMOUNT = 20;
 // Build a balanced Binary Search Tree with a sorted array of random numbers < 100
 const bst = new Tree(getNumbers(AMOUNT));
 
-bst.printTree();
+// Visualize tree
+prettyPrint(bst.root);
