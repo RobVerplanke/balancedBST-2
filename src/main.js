8b1799c39e1c71b3c7c5467179af908f6bb7a2d5
Rob <rverplanke@gmail.com>
2024-01-19 16:40:12 +0100

    implement bfs function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import getNumbers from './modules/data-generator/generate-numbers.js';
-import Tree from './modules/bst.js';
+import Tree, { find, levelOrderIteration } from './modules/bst.js';
 import prettyPrint from './prettyPrint.js';
 
 // The desired amount of nodes in the tree
 const AMOUNT = 20;
+const SEARCHVALUE = 68;
 
 // Convert a sorted array of random numbers (<100) to a balanced Binary Search Tree
 const bst = new Tree(getNumbers(AMOUNT));
@@ -11,4 +12,11 @@ const bst = new Tree(getNumbers(AMOUNT));
 // Visualize the tree
 prettyPrint(bst.root);
 
-console.log(bst.find(4));
+if (find(SEARCHVALUE, bst.root) === null) {
+  console.log(`\nNode ${SEARCHVALUE} not found!`);
+} else {
+  console.log(`\nFound node ${SEARCHVALUE}: ${find(SEARCHVALUE, bst.root)}`);
+}
+
+console.log(`\nLevel order: ${levelOrderIteration(bst.root)}`);
+
--- src/modules/bst.js
+++ src/modules/bst.js
@@ -23,101 +23,130 @@ export default class Tree {
   constructor(arr) {
     this.root = buildTree(arr);
   }
+}
+// Write insert and delete functions that accepts a value to insert/delete.
+// You’ll have to deal with several cases for delete, such as when a node has children or not.
 
-  // Write insert and delete functions that accepts a value to insert/delete.
-  // You’ll have to deal with several cases for delete, such as when a node has children or not.
+// insert(value) {
 
-  // insert(value) {
+// }
 
-  // }
+// delete(value) {
 
-  // delete(value) {
-
-  // }
+// }
 
 
-  // Write a find function that accepts a value and returns the node with the given value.
+// Write a find function that accepts a value and returns the node with the given value.
 
-  find(value, root = this.root) {
+export function find(value, root) {
 
-    // Base Case: check if the value is present in the tree or if the root already has the value.
-    if (root === null || value === root.data) return root;
+  // Base Case: check if the value is present in the tree or if the root already has the value.
+  if (root === null || value === root.data) return root;
 
-    // Value is smaller than current root's data: iterate through left subtree
-    if (value < root.data) return this.find(value, root.left);
+  // Value is smaller than current root's data: iterate through left subtree
+  if (value < root.data) return find(value, root.left);
 
-    // Value is greater than current root's data: iterate through right subtree
-    if (value > root.data) return this.find(value, root.right);
+  // Value is greater than current root's data: iterate through right subtree
+  if (value > root.data) return find(value, root.right);
 
-    return null;
+  return null;
+}
+
+
+// Write a levelOrder function that accepts a random optional callback function as its parameter.
+// levelOrder should traverse the tree in breadth-first level order and provide each node as
+// an argument to the callback.
+// As a result, the callback will perform an operation on each node following the order
+// in which they are traversed.
+// levelOrder may be implemented using either iteration or recursion (try implementing both!).
+// The method should return an array of values if no callback is given as an argument.
+
+// Bfs using iteration
+export function levelOrderIteration(root, callback) {
+  const result = [];
+  const queue = [];
+
+  // Set root as first item in queue
+  if (root) queue.push(root);
+
+  // As long as there are items in the queue
+  while (queue.length) {
+
+    // Get first item from queue
+    const currentNode = queue.shift();
+
+    // Add its value to result list
+    result.push(currentNode.data);
+
+    // Use callback
+    // code here
+
+    // Push left node to queue
+    if (currentNode.left) queue.push(currentNode.left);
+
+    // Push right node to queue
+    if (currentNode.right) queue.push(currentNode.right);
+
   }
 
-
-  // Write a levelOrder function that accepts a random optional callback function
-  // as its parameter. levelOrder should traverse the tree in breadth-first level
-  // order and provide each node as an argument to the callback.
-  // As a result, the callback will perform an operation on each node following the order
-  // in which they are traversed.
-  // levelOrder may be implemented using either iteration or recursion
-  // (try implementing both!).
-  // The method should return an array of values if no callback is given as an argument.
-  // Tip: You will want to use an array acting as a queue to keep track of all the
-  // child nodes that you have yet to traverse and to add new ones to the list
-
-  // levelOrder(callback) {
-
-  // }
-
-
-  // Write inOrder, preOrder, and postOrder functions that also accept a random optional
-  // callback as a parameter. Each of these functions should traverse the tree in their
-  // respective depth-first order and yield each node to the provided callback.
-  // The functions should return an array of values if no callback is given as an argument.
-
-  // inOrder() {
-
-  // }
-
-  // preOrder() {
-
-  // }
-
-  // postOrder() {
-
-  // }
-
-
-  // Write a height function that accepts a node and returns its height.
-  // Height is defined as the number of edges in the longest path from a given node
-  // to a leaf node.
-
-  // height() {
-
-  // }
-
-
-  // Write a depth function that accepts a node and returns its depth.
-  // Depth is defined as the number of edges in the path from a given node to the tree’s root node.
-
-  // depth() {
-
-  // }
-
-
-  // Write an isBalanced function that checks if the tree is balanced.
-  // A balanced tree is one where the difference between heights of the left subtree and
-  // the right subtree of every node is not more than 1.
-
-  // isBalanced() {
-
-  // }
-
-
-  // Write a rebalance function that rebalances an unbalanced tree.
-  // Tip: You’ll want to use a traversal method to provide a new array to the buildTree function.
-
-  // rebalance() {
-
-  // }
-
+  return result;
 }
+
+
+// Bfs using iteration
+// export function levelOrderRecursion(root) {
+
+// }
+
+
+// Write inOrder, preOrder, and postOrder functions that also accept a random optional
+// callback as a parameter. Each of these functions should traverse the tree in their
+// respective depth-first order and yield each node to the provided callback.
+// The functions should return an array of values if no callback is given as an argument.
+
+// inOrder() {
+
+// }
+
+// preOrder() {
+
+// }
+
+// postOrder() {
+
+// }
+
+
+// Write a height function that accepts a node and returns its height.
+// Height is defined as the number of edges in the longest path from a given node
+// to a leaf node.
+
+// height() {
+
+// }
+
+
+// Write a depth function that accepts a node and returns its depth.
+// Depth is defined as the number of edges in the path from a given node to the tree’s root node.
+
+// depth() {
+
+// }
+
+
+// Write an isBalanced function that checks if the tree is balanced.
+// A balanced tree is one where the difference between heights of the left subtree and
+// the right subtree of every node is not more than 1.
+
+// isBalanced() {
+
+// }
+
+
+// Write a rebalance function that rebalances an unbalanced tree.
+// Tip: You’ll want to use a traversal method to provide a new array to the buildTree function.
+
+// rebalance() {
+
+// }
+
